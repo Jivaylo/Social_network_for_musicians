c752d3da4a11144e82c557de8dfaef500ae36e83
Ivaylo <ivaylo06@abv.bg>
2024-12-10 11:22:37 +0200

    Initial commit

---
+++ SocialNetworkForMusician/SocialNetworkForMusician/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SocialNetworkForMusician.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
