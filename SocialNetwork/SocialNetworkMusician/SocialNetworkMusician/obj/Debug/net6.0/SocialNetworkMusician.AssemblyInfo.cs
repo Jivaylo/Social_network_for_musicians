e2987d0e688f6076b3d2f561d3d685d8d0c80c1b
Ivaylo <ivaylo06@abv.bg>
2025-04-09 21:34:06 +0300

    add navbar

--- SocialNetwork/SocialNetworkMusician/SocialNetworkMusician/obj/Debug/net6.0/SocialNetworkMusician.AssemblyInfo.cs
+++ SocialNetwork/SocialNetworkMusician/SocialNetworkMusician/obj/Debug/net6.0/SocialNetworkMusician.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SocialNetworkMusician")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce23290127253470f51c3b77297b31d7ec1c0ade")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4401ff31afaf2dadccfbc73668b44c40731d6151")]
 [assembly: System.Reflection.AssemblyProductAttribute("SocialNetworkMusician")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SocialNetworkMusician")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
