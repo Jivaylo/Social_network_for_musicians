5835dd8e6cbe1ba2081257be1cad01d0b7444824
Ivaylo <ivaylo06@abv.bg>
2025-04-09 23:06:44 +0300

    add some things

--- SocialNetwork/SocialNetworkMusician/SocialNetworkMusician/obj/Debug/net6.0/SocialNetworkMusician.AssemblyInfo.cs
+++ SocialNetwork/SocialNetworkMusician/SocialNetworkMusician/obj/Debug/net6.0/SocialNetworkMusician.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SocialNetworkMusician")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95219669ab7a1a579ed36f0539f971b74cab1acd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1095e9bb12685966e1285722f83300d9129cb1e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("SocialNetworkMusician")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SocialNetworkMusician")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
